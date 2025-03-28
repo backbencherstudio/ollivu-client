c43219c9bf1124f39682ccc594193f317c8918fa
tushar <tushar789imran@gmail.com>
2025-03-28 16:03:16 +0600

    dashboard layout done

--- app/(dashboard)/dashboard/layout.tsx
+++ app/(dashboard)/dashboard/layout.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import Header from "./_components/header";
+import Sidebar from "./_components/sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <div className="flex min-h-screen bg-[#F5FFFD]">
+      <Sidebar />
+      
+      <div className="flex-1">
+        <Header />
+        <main className="p-6">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
 }
