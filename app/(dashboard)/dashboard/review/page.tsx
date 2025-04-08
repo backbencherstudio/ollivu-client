f446cace4230dd356ba64f16222eb65a48289e54
alimuzzaman-zamaddar <alimuzzamanzamaddar@gmail.com>
2025-04-08 08:28:16 +0600

    done

--- app/(dashboard)/dashboard/review/page.tsx
+++ app/(dashboard)/dashboard/review/page.tsx
@@ -74,8 +74,6 @@ export default function AdminReviewsPage() {
           <RatingBreakdown />
         </div>
       </div>
-
-
       {/* Sort + Header */}
       <div className="flex items-center justify-between mt-8 border-b pb-3">
         <h2 className="text-base font-semibold">All reviews</h2>
