598a4caa1bcea112e0d4b88eefa4570f175570f1
tushar <tushar789imran@gmail.com>
2025-04-28 16:53:35 +0600

    search result implement and running

--- src/redux/features/users/userApi.ts
+++ src/redux/features/users/userApi.ts
@@ -10,6 +10,14 @@ export const usersApi = baseApi.injectEndpoints({
       providesTags: ["User"],
     }),
 
+    getAllUsersByService: builder.query({
+      query: (service) => ({
+        url: `/auth/allUsers?my_service=${service}`,
+        method: 'GET',
+      }),
+      providesTags: ['User'],
+    }),
+
     getSingleUser: builder.query({
       query: (id: string) => ({
         url: `/auth/${id}`,
@@ -88,13 +96,22 @@ export const usersApi = baseApi.injectEndpoints({
         method: "DELETE",
       }),
       invalidatesTags: ["User"],
-    })
+    }),
+
+    searchUsers: builder.query({
+      query: (searchTerm) => ({
+        url: `/auth/allUsers?searchTerm=${searchTerm}`,
+        method: 'GET',
+      }),
+      providesTags: ['User'],
+    }),
 
   }),
 });
 
 export const {
   useGetAllUsersQuery,
+  useGetAllUsersByServiceQuery,
   useGetSingleUserQuery,
   useGetCurrentUserQuery,
   useUpdateUserMutation,
@@ -103,5 +120,6 @@ export const {
   useUploadPortfolioMutation,
   useDeletePortfolioMutation,
   useUploadCertificateMutation,
-  useDeleteCertificateMutation
+  useDeleteCertificateMutation,
+  useSearchUsersQuery,
 } = usersApi;
