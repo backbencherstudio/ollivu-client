27366b5ec00b7a8c07f47e3eb38b8c700760660b
FozleRabbi1 <fozlerabbishuvo@gmail.com>
2025-05-14 12:10:00 +0600

    update

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['backend.ollivu.com','192.168.4.42', '192.168.5.6','192.168.4.56', '192.168.0.107'],
+    domains: ['backend.ollivu.com','localhost','192.168.4.42', '192.168.5.6','192.168.4.56', '192.168.0.107'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'backend.ollivu.com',
+        pathname: '/uploads/',
+       },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '5000',
         pathname: '/uploads/',
        },
       {
@@ -17,7 +23,7 @@ const nextConfig = {
       {
         protocol: 'http',
         hostname: '192.168.5.6',
-        port: '5000',
+        port: '5000', 
         pathname: '/uploads/',
       },
       {
