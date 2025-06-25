83a0e5a1a0bf3f71634e7c52ad8b63f12518e8a1
tushar <tushar789imran@gmail.com>
2025-06-25 12:46:46 +0600

    profile status text style change

--- app/(dashboard)/dashboard/monitor-users/components/ConversationTable.tsx
+++ app/(dashboard)/dashboard/monitor-users/components/ConversationTable.tsx
@@ -26,7 +26,7 @@ export function ConversationTable({
   setOpen,
   onStatusChange,
 }: ConversationTableProps) {
-  // console.log("conversations", conversations);
+  console.log("conversations", conversations);
 
   const [viewDetailsModal, setViewDetailsModal] = useState<{
     isOpen: boolean;
@@ -50,32 +50,72 @@ export function ConversationTable({
               <tr className="text-left text-gray-500 border-b bg-gray-50">
                 {isSuspendedView ? (
                   <>
-                    <th className="py-2 sm:py-3 px-2 sm:px-4 font-medium whitespace-nowrap">#</th>
-                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">User</th>
-                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">Email</th>
-                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">Reason</th>
-                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">Suspended Date</th>
-                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">Status</th>
-                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">Action</th>
+                    <th className="py-2 sm:py-3 px-2 sm:px-4 font-medium whitespace-nowrap">
+                      #
+                    </th>
+                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">
+                      User
+                    </th>
+                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">
+                      Email
+                    </th>
+                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">
+                      Reason
+                    </th>
+                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">
+                      Suspended Date
+                    </th>
+                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">
+                      Status
+                    </th>
+                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">
+                      Action
+                    </th>
                   </>
                 ) : isReportedView ? (
                   <>
-                    <th className="py-2 sm:py-3 px-2 sm:px-4 font-medium whitespace-nowrap">#</th>
-                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">Send Report User</th>
-                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">Receive Report User</th>
-                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">Reason</th>
-                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">Report Date</th>
-                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">Status</th>
-                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">Action</th>
+                    <th className="py-2 sm:py-3 px-2 sm:px-4 font-medium whitespace-nowrap">
+                      #
+                    </th>
+                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">
+                      Send Report User
+                    </th>
+                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">
+                      Receive Report User
+                    </th>
+                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">
+                      Reason
+                    </th>
+                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">
+                      Report Date
+                    </th>
+                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">
+                      Status
+                    </th>
+                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">
+                      Action
+                    </th>
                   </>
                 ) : (
                   <>
-                    <th className="py-2 sm:py-3 px-2 sm:px-4 font-medium whitespace-nowrap">#</th>
-                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">Sender Service</th>
-                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">Receiver Service</th>
-                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">Status</th>
-                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">Join Date</th>
-                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">Action</th>
+                    <th className="py-2 sm:py-3 px-2 sm:px-4 font-medium whitespace-nowrap">
+                      #
+                    </th>
+                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">
+                      Sender Service
+                    </th>
+                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">
+                      Receiver Service
+                    </th>
+                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">
+                      Status
+                    </th>
+                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">
+                      Join Date
+                    </th>
+                    <th className="px-2 sm:px-4 font-medium whitespace-nowrap">
+                      Action
+                    </th>
                   </>
                 )}
               </tr>
@@ -83,13 +123,23 @@ export function ConversationTable({
             <tbody>
               {conversations.map((item, index) => (
                 <tr key={item.id} className="border-b hover:bg-gray-50">
-                  <td className="py-2 sm:py-4 px-2 sm:px-4 whitespace-nowrap">{index + 1}</td>
+                  <td className="py-2 sm:py-4 px-2 sm:px-4 whitespace-nowrap">
+                    {index + 1}
+                  </td>
                   {isSuspendedView ? (
                     <>
-                      <td className="px-2 sm:px-4 whitespace-nowrap">{item?.reportedId.first_name}</td>
-                      <td className="px-2 sm:px-4 whitespace-nowrap">{item?.reportedId.email}</td>
-                      <td className="px-2 sm:px-4 whitespace-nowrap">{item?.reportType}</td>
-                      <td className="px-2 sm:px-4 whitespace-nowrap">{item.updatedAt}</td>
+                      <td className="px-2 sm:px-4 whitespace-nowrap">
+                        {item?.reportedId.first_name}
+                      </td>
+                      <td className="px-2 sm:px-4 whitespace-nowrap">
+                        {item?.reportedId.email}
+                      </td>
+                      <td className="px-2 sm:px-4 whitespace-nowrap">
+                        {item?.reportType}
+                      </td>
+                      <td className="px-2 sm:px-4 whitespace-nowrap">
+                        {item.updatedAt}
+                      </td>
                       <td className="px-2 sm:px-4 whitespace-nowrap">
                         <span className="text-[10px] sm:text-xs px-2 py-1 rounded-full font-medium bg-red-100 text-yellow-600">
                           {item?.action}
@@ -99,7 +149,12 @@ export function ConversationTable({
                         <Button
                           variant="link"
                           className="p-0 text-[#4A4C56] hover:text-[#20B894] cursor-pointer text-[10px] sm:text-xs"
-                          onClick={() => setViewDetailsModal({ isOpen: true, conversation: item })}
+                          onClick={() =>
+                            setViewDetailsModal({
+                              isOpen: true,
+                              conversation: item,
+                            })
+                          }
                         >
                           View details
                         </Button>
@@ -159,18 +214,35 @@ export function ConversationTable({
                           {item?.reportedId?.first_name || "Anonymous"}
                         </div>
                       </td>
-                      <td className="px-2 sm:px-4 whitespace-nowrap">{item.reason}</td>
                       <td className="px-2 sm:px-4 whitespace-nowrap">
-                        <p className="text-[#4A4C56] text-[10px] sm:text-xs">{item.createdAt}</p>
+                        {item.reason}
                       </td>
                       <td className="px-2 sm:px-4 whitespace-nowrap">
-                        <p className="text-[#4A4C56] text-[10px] sm:text-xs">{item.status}</p>
+                        <p className="text-[#4A4C56] text-[10px] sm:text-xs">
+                          {item.createdAt}
+                        </p>
+                      </td>
+                      <td className="px-2 sm:px-4 whitespace-nowrap">
+                        <p
+                          className={`text-[#4A4C56] text-[10px] sm:text-xs ${
+                            item.status === "blocked"
+                              ? "text-red-500 font-bold"
+                              : ""
+                          }`}
+                        >
+                          {item.status}
+                        </p>
                       </td>
                       <td className="px-2 sm:px-4 whitespace-nowrap">
                         <Button
                           variant="link"
                           className="p-0 text-[#4A4C56] hover:text-[#20B894] cursor-pointer text-[10px] sm:text-xs"
-                          onClick={() => setViewDetailsModal({ isOpen: true, conversation: item })}
+                          onClick={() =>
+                            setViewDetailsModal({
+                              isOpen: true,
+                              conversation: item,
+                            })
+                          }
                         >
                           View details
                         </Button>
@@ -178,8 +250,12 @@ export function ConversationTable({
                     </>
                   ) : (
                     <>
-                      <td className="px-2 sm:px-4 whitespace-nowrap">{item.user1}</td>
-                      <td className="px-2 sm:px-4 whitespace-nowrap">{item.user2}</td>
+                      <td className="px-2 sm:px-4 whitespace-nowrap">
+                        {item.user1}
+                      </td>
+                      <td className="px-2 sm:px-4 whitespace-nowrap">
+                        {item.user2}
+                      </td>
                       <td className="px-2 sm:px-4 whitespace-nowrap">
                         <p
                           className={`text-[10px] sm:text-xs px-2 py-1 rounded-full font-medium text-center ${
@@ -192,13 +268,20 @@ export function ConversationTable({
                         </p>
                       </td>
                       <td className="px-2 sm:px-4 whitespace-nowrap">
-                        <p className="text-[#4A4C56] text-[10px] sm:text-xs">{item.joinDate}</p>
+                        <p className="text-[#4A4C56] text-[10px] sm:text-xs">
+                          {item.joinDate}
+                        </p>
                       </td>
                       <td className="px-2 sm:px-4 whitespace-nowrap">
                         <Button
                           variant="link"
                           className="p-0 text-[#4A4C56] hover:text-[#20B894] cursor-pointer text-[10px] sm:text-xs"
-                          onClick={() => setViewDetailsModal({ isOpen: true, conversation: item })}
+                          onClick={() =>
+                            setViewDetailsModal({
+                              isOpen: true,
+                              conversation: item,
+                            })
+                          }
                         >
                           View details
                         </Button>
@@ -216,7 +299,9 @@ export function ConversationTable({
         isOpen={viewDetailsModal.isOpen}
         suspendData={[viewDetailsModal?.conversation]} // Only pass the selected user's data
         setViewDetailsModal={setViewDetailsModal}
-        onClose={() => setViewDetailsModal({ isOpen: false, conversation: null })}
+        onClose={() =>
+          setViewDetailsModal({ isOpen: false, conversation: null })
+        }
         conversation={viewDetailsModal?.conversation}
         isReportedView={isReportedView}
         isSuspendedView={isSuspendedView}
